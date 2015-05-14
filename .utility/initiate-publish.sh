89284c634efd0f79fb64f5a25df93a068eb7e9f0
Nisha <nisha@palomamobile.com>
2015-05-14 16:33:17 +1000

    initiate.publish script added block for snapshot and release

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -2,21 +2,35 @@
 # This script initiates the Gradle publishing task when pushes to master occur.
 
 if [ "$TRAVIS_REPO_SLUG" == "PalomaMobile/android-db-migration" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
   if [[ $(gradle -q getVersion) != *SNAPSHOT* ]]; then
-      echo 'Travis can only publish snapshots.'
-      return 0
-  fi
 
-  echo -e "Starting publish to Sonatype...\n"
+    echo -e "Starting to publish release to Sonatype...\n"
 
-  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
-  RETVAL=$?
+      gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+      RETVAL=$?
 
-  if [ $RETVAL -eq 0 ]; then
-    echo 'Completed publish!'
+      if [ $RETVAL -eq 0 ]; then
+        echo 'Completed publish!'
+      else
+        echo 'Publish failed.'
+          return 1
+      fi
+
   else
-    echo 'Publish failed.'
-    return 1
+
+    echo -e "Starting to publish snapshot to Sonatype...\n"
+
+    gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+    RETVAL=$?
+
+    if [ $RETVAL -eq 0 ]; then
+      echo 'Completed snapshot publish!'
+    else
+      echo 'Snapshot Publish failed.'
+      return 1
+    fi
+
   fi
 
 fi
