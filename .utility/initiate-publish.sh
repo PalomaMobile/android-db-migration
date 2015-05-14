e8cf8f11fb84a1fc86e1909f7edebb98a5bef0c1
Nisha <nisha@palomamobile.com>
2015-05-14 15:39:22 +1000

    test snapshot release

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -2,14 +2,14 @@
 # This script initiates the Gradle publishing task when pushes to master occur.
 
 if [ "$TRAVIS_REPO_SLUG" == "PalomaMobile/android-db-migration" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+  if [[ $(gradle -q getVersion) != *SNAPSHOT* ]]; then
       echo 'Travis can only publish snapshots.'
       return 0
   fi
 
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
+  gradle uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}"
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
