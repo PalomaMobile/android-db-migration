5e722bdcd370976d9baa3f4167fb3deb987fd675
Nisha <nisha@palomamobile.com>
2015-05-14 14:02:10 +1000

    publish script added

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # This script initiates the Gradle publishing task when pushes to master occur.
 
-
 if [ "$TRAVIS_REPO_SLUG" == "PalomaMobile/android-db-migration" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
-      echo 'Travis can only publish snapshots. Publish a release, coming soon...'
+      echo 'Travis can only publish snapshots.'
       return 0
   fi
 
